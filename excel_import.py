4f06f497ff8d30e4588c6c13f0b11c9b66017a3c
Ole Hissbach <ohissbach@talentschmiede.com>
2023-04-03 13:49:12 +0200

    removed clutter

--- excel_import.py
+++ excel_import.py
@@ -14,7 +14,7 @@ def read_excel_to_list(file_path: str, sheet_name: str) -> list:
     """
     try:
         # Load the Excel sheet into a pandas DataFrame
-        excel_data = pd.read_excel(file_path, sheet_name=sheet_name)
+        excel_data = pd.read_excel(file_path, sheet_name)
         # Check if the sheet is empty
         if excel_data.empty:
             print(f"The sheet '{sheet_name}' in '{file_path}' is empty.")
@@ -29,10 +29,10 @@ def read_excel_to_list(file_path: str, sheet_name: str) -> list:
         print(f"An error occurred while reading the file '{file_path}': {str(e)}")
         return []
     
-    
+
     # Convert all values in the DataFrame to strings
     excel_data = excel_data.astype(str)
     # Convert the DataFrame to a list of strings
     value_list = [val for val in excel_data.values.flatten() if str(val) != 'nan']
-
+    print(value_list)
     return value_list
