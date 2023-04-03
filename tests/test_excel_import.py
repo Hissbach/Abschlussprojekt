5b86166e7fb23b570516c81e13249b55e1f52be6
Ole Hissbach <ohissbach@talentschmiede.com>
2023-04-03 15:08:38 +0200

    Adjusted testcase for my excel_import

--- tests/test_excel_import.py
+++ tests/test_excel_import.py
@@ -30,7 +30,8 @@ class TestReadExcelToList(unittest.TestCase):
         mock_data = pd.DataFrame({'A': [1, 2], 'B': [3, 4]})
         mock_read_excel.return_value = mock_data
         result = read_excel_to_list('file.xlsx', 'Sheet1')
-        self.assertEqual(result, [1, 3, 2, 4])
+        self.assertEqual(result, ['1', '3', '2', '4'])
+
     @patch('pandas.read_excel')
     def test_read_excel_nonexistent_sheet(self, mock_read_excel):
         mock_read_excel.side_effect = ValueError("No sheet named 'NonexistentSheet'")
